a878596ee2451752466999043cefe6322c6325e4
Erik Nestorovic <enestorovic@gmail.com>
2021-04-28 18:05:41 -0400

    add API and authorization

--- backend/handler.rb
+++ backend/handler.rb
@@ -1,6 +1,52 @@
 require 'json'
+require 'httparty'
 
 def whoami(event:, context:)
 	hash = {username: 'edn28'}
 	{ statusCode: 200, body: JSON.generate(hash) }
+end
+
+def recipes(event:, context:)
+	response = nil
+
+
+	puts "EVENT: "
+	puts event["headers"]
+
+	if event["headers"]["Authorization"]
+		token = event["headers"]["Authorization"]
+	else
+		token = false
+	end
+
+	if(!token)
+		response = {statusCode: 401}
+	else
+		#verify in firebase
+		begin
+			verification = firebase_verification(token)
+			if verification["users"][0]["localId"]
+				# User has been verified
+				hash = [{title: 'Foolproof pan pizza',description: 'Try this amazing pan pizza recipe!',voteTotal: 35},{title: 'Foolproof pan pizza #2',description: 'Try this second pan pizza recipe!',voteTotal: 50}]
+				response = { statusCode: 200, body: JSON.generate(hash) }
+			else
+				response = {statusCode: 401}
+			end
+		rescue
+			response = {statusCode: 401}
+		end
+	end
+	
+	response
+	
+end
+
+def firebase_verification(token)
+	url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/getAccountInfo?key=AIzaSyDbXZCw-a5eWxEm1RbJIg45vc45ALbzUVg"
+	firebase_verification_call = HTTParty.post(url, headers: { 'Content-Type' => 'application/json' }, body: { 'idToken' => token }.to_json )
+	if firebase_verification_call.response.code == "200"
+		firebase_infos = firebase_verification_call.parsed_response
+	else
+		raise StandardError.new "Invalid token"
+	end
 end
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,102 +14,141 @@ const config = {
 };
 
 firebase.initializeApp(config);
- 
-// Configure FirebaseUI.
-const uiConfig = {
-	// Popup signin flow rather than redirect flow.
-	signInFlow: 'popup',
-	// Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-	signInSuccessUrl: '/signedIn',
-	// We will display Google and Facebook as auth providers.
-	signInOptions: [
-		firebase.auth.EmailAuthProvider.PROVIDER_ID
-	],
-	callbacks: {
-    // Avoid redirects after sign-in.
-    signInSuccessWithAuthResult: () => false,
-  }
-};
 
-function SignInScreen() {
-	const [isSignedIn, setIsSignedIn] = useState(false); // Local signed-in state.
+class SignedInComponent extends React.Component {
+	constructor(props) {
+		super(props);
 
-	// Listen to the Firebase Auth state and set the local state.
-	useEffect(() => {
-		const unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
-			setIsSignedIn(!!user);
-		});
-		return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
-	}, []);
+		this.state = {
+			recipes: null,
+		};
+	}
 
-	if (!isSignedIn) {
+	async componentDidMount() {
+		const token = await firebase.auth().currentUser?.getIdToken();
+		console.log(token);
+
+		try {
+			const response = await fetch("http://localhost:4000/dev/recipes", {
+				headers: {
+					Authorization: token,
+				},
+			});
+			if (response.status === 401) {
+				console.log("unauthorized");
+			} else {
+				const data = await response.json();
+				this.setState({ recipes: data });
+			}
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	signOut() {
+		firebase.auth().signOut();
+	}
+
+	render() {
+		const userName = firebase.auth().currentUser?.email;
+		return (
+			<section className="section">
+				<div className="container has-text-centered">
+					Hello {userName}! You're now signed in
+					<div className="title">Recipes</div>
+						{
+							this.state.recipes && this.state.recipes.map(recipe => {
+								return (
+									<div className="box columns">
+										<div>
+												<span className="icon">
+													<span className="fa-stack">
+														<i className="fa-stack fa-2x	"><FontAwesomeIcon icon="caret-up" /></i>
+														<i className="fa-stack fa-2x"><FontAwesomeIcon icon="caret-down" /></i>
+													</span>
+												</span>
+										</div>
+										<article className="media">
+											<div className="media-left">
+												<figure className="image is-64x64">
+													<img src="https://bulma.io/images/placeholders/128x128.png" alt="Image"></img>
+												</figure>
+											</div>
+											<div className="media-content">
+												<div className="content">
+													<p>
+														<strong>{recipe.title}</strong>
+													</p>
+													<p>
+														{recipe.description}
+													</p>
+												</div>
+											</div>
+										</article>
+									</div>
+								);
+							})
+						}
+					<button onClick={() => this.signOut()}>Sign out</button>
+				</div>
+			</section>
+		);
+	}
+}
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			user: null,
+		};
+	}
+
+	uiConfig = {
+		// Popup signin flow rather than redirect flow.
+		signInFlow: 'popup',
+		// Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+		signInSuccessUrl: '/signedIn',
+		// We will display Google and Facebook as auth providers.
+		signInOptions: [
+			firebase.auth.EmailAuthProvider.PROVIDER_ID
+		],
+		callbacks: {
+			// Avoid redirects after sign-in.
+			signInSuccessWithAuthResult: () => false,
+		}
+	};
+
+
+	componentDidMount() {
+		this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
+			(user) => this.setState({ isSignedIn: !!user })
+		);
+	}
+
+	componentDidUnmount() {
+		this.unregisterAuthObserver();
+	}
+
+	render(){
+		if (this.state.isSignedIn) {
+      return <SignedInComponent />;
+    }
 		return (
 			<div className="App container">
 				<h1>My App</h1>
-				<p>Please sign-in:</p>
-				<StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+
+				{this.state.isSignedIn !== undefined && !this.state.isSignedIn &&
+					<div>
+						<p>Please sign-in:</p>
+						<StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()} />
+					</div>
+				}
 			</div>
 		);
 	}
-	return (
-		<div className="App container">
-			<h1>My App</h1>
-			<p>Welcome {firebase.auth().currentUser.displayName} | {firebase.auth().currentUser.email}! You are now signed-in!</p>
-			<a onClick={() => firebase.auth().signOut()}>Sign-out</a>
-
-			<div>
-				<h1 className="title">Dashboard</h1>
-
-				<div className="box columns">
-					<div>
-							<span className="icon">
-								<span className="fa-stack">
-									<i className="fa-stack fa-2x	"><FontAwesomeIcon icon="caret-up" /></i>
-									<i className="fa-stack fa-2x"><FontAwesomeIcon icon="caret-down" /></i>
-								</span>
-							</span>
-					</div>
-					<article className="media">
-						<div className="media-left">
-							<figure className="image is-64x64">
-								<img src="https://bulma.io/images/placeholders/128x128.png" alt="Image"></img>
-							</figure>
-						</div>
-						<div className="media-content">
-							<div className="content">
-								<p>
-									<strong>Floolproof Pan Pizza</strong>
-								</p>
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
-								</p>
-							</div>
-						</div>
-					</article>
-				</div>
-
-				<div className="box">
-					<article className="media">
-						<div className="media-left">
-							<figure className="image is-64x64">
-								<img src="https://bulma.io/images/placeholders/128x128.png" alt="Image"></img>
-							</figure>
-						</div>
-						<div className="media-content">
-							<div className="content">
-								<p>
-									<strong>Floolproof Pan Pizza</strong>
-								</p>
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
-								</p>
-							</div>
-						</div>
-					</article>
-				</div>
-			</div>
-		</div>
-	);
+	
 }
 
-export default SignInScreen
+export default App
